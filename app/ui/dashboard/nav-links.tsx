d8845e86758b7bf6dbdcaa3b6edb9c9728b8d08e
Mo <65149031+AE542@users.noreply.github.com>
2024-08-19 09:52:51 +0100

    initial commit

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,8 +1,15 @@
+'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+// to allow active links to show up on page:
+import { usePathname } from 'next/navigation';
+// importing clsx for conditionally applying class names will help here
+import clsx from 'clsx';
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -17,19 +24,27 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname();
+  // now we can pass this below to the link
   return (
     <>
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            // adding the active class to the link when the pathname matches the href
+            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            {
+              'bg-sky-100 text-blue-600': pathname === link.href,
+            },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
+          // changing from a tag to Link prevents full page reload between dashboard pages
         );
       })}
     </>
